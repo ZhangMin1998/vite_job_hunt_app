24861fc43244a35751d144e47c1e0fdfc8708b34
ZhangMin1998 <1464253423@qq.com>
2023-11-29 00:28:23 +0800

    底部导航公共组件

--- src/components/FooterTabbar.vue
+++ src/components/FooterTabbar.vue
@@ -1,1 +1,95 @@
-<template></template>
+<template>
+  <dl>
+    <dt class="icon-task-bar" :class="route.path == '/task' ? 'active' : ''" @click="gotoPage('/task')">
+      <i></i>
+      <p>任务</p>
+    </dt>
+    <dt class="icon-contract-bar" :class="route.path == '/contract' ? 'active' : ''" @click="gotoPage('/contract')">
+      <i></i>
+      <p>合约</p>
+    </dt>
+    <dt class="icon-message-bar" :class="route.path == '/message' ? 'active' : ''" @click="gotoPage('/message')">
+      <i></i>
+      <p>消息</p>
+    </dt>
+    <dt class="icon-my-bar" :class="route.path == '/my' ? 'active' : ''" @click="gotoPage('/my')">
+      <i></i>
+      <p>我的</p>
+    </dt>
+  </dl>
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const gotoPage = (path: string) => {
+  router.push(path)
+}
+
+</script>
+
+<style lang="less" scoped>
+dl{
+  display: flex;
+  width: 100%;
+  height: 3rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  border-top: 1px solid #dddddd;
+  background: #ffffff;
+  dt{
+    flex: 1;
+    padding: 0.69rem 0;
+    justify-content: center;
+    text-align: center;
+    font-size: 0.59rem;
+    font-family: PingFang SC;
+    font-weight: 400;
+    color: #666666;
+    &.active p {
+      color: #ff9415;
+    }
+    i{
+      width: 0.91rem;
+      height: 0.91rem;
+      display: block;
+      margin: 0 auto;
+    }
+  }
+}
+.icon-task-bar i{
+  background: url('@/assets/img/icon/bar-task-link.png') no-repeat;
+  background-size: 100%;
+}
+.icon-task-bar.active i{
+    background: url('@/assets/img/icon/bar-task-active.png') no-repeat;
+    background-size: 100%;
+}
+.icon-contract-bar i{
+  background: url('@/assets/img/icon/bar-contract-link.png') no-repeat;
+  background-size: 100%;
+}
+.icon-contract-bar.active i{
+    background: url('@/assets/img/icon/bar-contract-active.png') no-repeat;
+    background-size: 100%;
+}
+.icon-message-bar i{
+  background: url('@/assets/img/icon/bar-message-link.png') no-repeat;
+  background-size: 100%;
+}
+.icon-message-bar.active i{
+    background: url('@/assets/img/icon/bar-message-active.png') no-repeat;
+    background-size: 100%;
+}
+.icon-my-bar i{
+  background: url('@/assets/img/icon/bar-my-link.png') no-repeat;
+  background-size: 100%;
+}
+.icon-my-bar.active i{
+    background: url('@/assets/img/icon/bar-my-active.png') no-repeat;
+    background-size: 100%;
+}
+</style>
--- src/views/task/index.vue
+++ src/views/task/index.vue
@@ -1,1 +1,11 @@
-<template></template>
+<template>
+  <FooterTabbar></FooterTabbar>
+</template>
+
+<script setup lang="ts">
+import FooterTabbar from '@/components/FooterTabbar.vue'
+</script>
+
+<style lang="less" scoped>
+  
+</style>
