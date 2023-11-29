251fcc34913dab422759c40858bed97762a901df
ZhangMin1998 <1464253423@qq.com>
2023-11-29 14:56:45 +0800

    隐私政策页面与接口

--- src/views/login/privacyPolicy.vue
+++ src/views/login/privacyPolicy.vue
@@ -1,1 +1,39 @@
-<template></template>
+<template>
+  <van-nav-bar
+    title="隐私政策"
+    left-text=""
+    left-arrow
+    @click-left="onClickLeft"
+  />
+  <div class="text-agree" v-html="state.htmlText"></div>
+</template>
+
+<script setup lang="ts">
+import { getPolicy } from '@/api/user'
+import { showToast } from 'vant'
+
+const state = reactive({
+  htmlText: ''
+})
+const onClickLeft = () => history.back()
+
+const getPolicyChange = async () => {
+  const res = await getPolicy({
+    id: 16
+  })
+  if (res) {
+    state.htmlText = (res as any).records[0].content
+  } else {
+    showToast((res as any).msg)
+  }
+}
+onMounted(() => {
+  getPolicyChange()
+})
+</script>
+
+<style lang="less" scoped>
+.text-agree{
+  padding: 0.5rem 0.7rem;
+}
+</style>
