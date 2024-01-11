bdf3c4ea7ecb52c3948c62571284c34df4575e41
ZhangMin1998 <1464253423@qq.com>
2024-01-11 22:21:01 +0800

    简历基本信息模块：基础模块组件设计与实现

--- src/views/my/resume/components/PersonInfo.vue
+++ src/views/my/resume/components/PersonInfo.vue
@@ -1,15 +1,72 @@
 <template>
   <div class="person_info">
-
+    <div class="left">
+      <h3>
+        前端
+        <!-- {{props.item.user_name}}
+        <img src="@/assets/img/my/icon-feedback.png" @click="state.show = true"  v-if="props.page!=='preview'" /> -->
+      </h3>
+      <!-- <p>{{props.item.work_year}} ｜ {{props.item.highest_education}} ｜ {{props.item.age}}</p> -->
+    </div>
+    <!-- <img :src="props.item.it_head" /> -->
   </div>
+
+  <!--切换城市弹窗-->
+  <van-popup v-model:show="state.citySwitchBool" position="left" duration="0.2" :style="{ width: '100%',height: '100%' }">
+    <!-- <CitySwitch></CitySwitch> -->
+  </van-popup>
 </template>
 
 <script setup lang="ts">
+import {myStore} from '@/store/my'
 
+const store = myStore()
+const props = defineProps({
+  item: {
+    type: Object
+  },
+  page: {
+    type: String
+  }
+})
+const state = reactive({
+  citySwitchBool: false
+})
 </script>
 
 <style lang="less" scoped>
 .person_info{
-  
+  display: flex;
+  justify-content: space-between;
+  padding: 0.99rem 0 1.41rem;
+  margin: 0 0.72rem;
+  border-bottom: 1px solid #F3F3F3;
+  .left{
+    h3{
+      font-size: 1.28rem;
+      line-height: 1.28rem;
+      font-weight: 400;
+      color: #333333;
+      margin-bottom: 0.64rem;
+      display: flex;
+      align-items: flex-end;
+      img{
+        width: 1rem;
+        height: 1rem;
+        margin-left: 0.32rem;
+      }
+    }
+    p{
+      font-size: 0.69rem;
+      line-height: 0.69rem;
+      font-weight: 300;
+      color: #666666;
+    }
+  }
+  img{
+    width: 3.19rem;
+    height: 3.19rem;
+    border-radius: 50%;
+  }
 }
 </style>
