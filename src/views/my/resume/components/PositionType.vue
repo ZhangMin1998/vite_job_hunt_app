edbb21df501909e52b28d3a8a6af6a2f4a70a5d5
ZhangMin1998 <1464253423@qq.com>
2024-01-16 22:12:18 +0800

    求职意愿模块：基于组件复用思想便捷实现职位类型设置功能

--- src/views/my/resume/components/PositionType.vue
+++ src/views/my/resume/components/PositionType.vue
@@ -1,9 +1,46 @@
 <template>
+  <div class="resume-label">
+    <h3>
+      求职意愿
+      <van-icon 
+        name="add-o" 
+        @click="state.show = true" 
+        v-if="props.page!=='preview'" 
+      />
+    </h3>
+    <label>请您填写求职意愿</label>
+  </div>
 
+  <van-popup 
+    v-model:show="state.show" 
+    position="left" 
+    duration="0.2" 
+    :style="{ width: '100%',height: '100%' }"
+  >
+    <PositionTypePopup></PositionTypePopup>
+  </van-popup>
 </template>
 
 <script setup lang="ts">
+import {myStore} from '@/store/my'
+import PositionTypePopup from './PositionTypePopup.vue'
 
+const store = myStore()
+const state = reactive({
+  show: false
+})
+const props = defineProps({
+  page: {
+    type: String
+  }
+})
+const closeChange = () => {
+  state.show = false
+  store.getResumeDetail()
+}
+provide('popup',{
+  closeChange
+})
 </script>
 
 <style lang="less" scoped>
---
+++ src/views/my/resume/components/PositionTypePopup.vue
@@ -0,0 +1,86 @@
+<template>
+  <van-nav-bar title="求职意愿" left-arrow @click-left="closeChange"/>
+  <div class="position_list">
+    <div class="user-item">
+      <h5>职位类型</h5>
+      <van-field 
+        v-model="state.positionValue" 
+        label="" 
+        placeholder="请选择您的职位类型" 
+        readonly
+        is-link
+        @click="state.showPositionType = true"
+      />
+    </div>
+    <div class="user-item">
+      <h5>服务类型</h5>
+      <van-field 
+        v-model="state.serviceValue" 
+        label="" 
+        placeholder="请选择您的服务类型" 
+        readonly
+        is-link
+        @click="state.showServiceType = true"
+      />
+    </div>
+    <div class="user-item">
+      <h5>服务价格</h5>
+      <van-field 
+        v-model="state.priceValue" 
+        label="" 
+        placeholder="请选择您的服务价格" 
+        readonly
+        is-link
+        @click="state.showPriceType = true"
+      />
+    </div>
+    <button class="wy-submit" @click="submit">保存</button>
+
+    <!--职位类型弹窗-->
+    <van-popup
+      v-model:show="state.showPositionType"
+      position="top" 
+      duration="0" 
+      :style="{ width: '100%',height: '100%' }"
+    >
+      <PositionType></PositionType>
+    </van-popup>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {myStore} from '@/store/my'
+import PositionType from '@/views/task/components/PositionType.vue'
+
+const store = myStore()
+const state = reactive({
+  showPositionType: false,
+  showServiceType: false,
+  showPriceType: false,
+  positionValue: '',
+  serviceValue: '',
+  priceValue: ''
+})
+const {closeChange} = inject('popup')
+const submit = async () => {
+
+}
+const closePositionType = (name) => {
+  if(name) state.positionValue = name
+  state.showPositionType = false
+}
+const closeServiceType = (name) => {
+  if(name) state.serviceValue = name
+  state.showServiceType = false
+}
+provide('popup',{
+  closePositionType,
+  closeServiceType
+})
+</script>
+
+<style lang="less" scoped>
+.position_list{
+  margin: 1.52rem 0.67rem 0;
+}
+</style>
