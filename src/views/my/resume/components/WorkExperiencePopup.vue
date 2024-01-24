33f2df08fee1eda609fde5ed0224d332746d535f
ZhangMin1998 <1464253423@qq.com>
2024-01-24 21:38:28 +0800

    工作经历模块：基于组件复用思想便捷实现职位类型设置功能

--- src/views/my/resume/components/WorkExperiencePopup.vue
+++ src/views/my/resume/components/WorkExperiencePopup.vue
@@ -1,166 +1,70 @@
 <template>
-  <van-nav-bar title="技能关键词" left-arrow @click-left="closeChange"/>
-  <dl class="skill-list">
-    <dt
-      @click="setSysSkill(index)" 
-      v-for="(item,index) in state.sysSkill" 
-      :key="index" 
-      :class="item.active ? 'active' : ''">
-      {{ item.title }}
-    </dt>
-    <dt
-      @click="setSkill(index)"
-      v-for="(item,index) in state.skillList" 
-      :key="index" 
-      :class="item.active ? 'active' : ''"
-    >
-      {{item.title}}
-      <van-icon @click.stop="clearSkill(index)" name="close" />
-    </dt>
-    <dt @click="state.show = true">自定义+</dt>
-  </dl>
-  <van-dialog 
-    v-model:show="state.show" 
-    title="自定义标签" 
-    show-cancel-button 
-    @confirm="skillChange"
-    @cancel="state.show=false"
-  >
-    <van-field
-      class="skill-field" 
-      v-model="state.value" 
-      label="" 
-      placeholder="请输入标签" 
-      maxlength="10" 
-    />
-  </van-dialog>
+  <van-nav-bar title="工作经历" left-arrow @click-left="closeChange"/>
+  <div class="position-list">
+    <div class="user-item">
+      <h5>公司名称</h5>
+      <van-field v-model="state.companyName" label="" placeholder="请填写您的公司名称" />
+    </div>
+    <div class="user-item">
+      <h5>职位类型</h5>
+      <van-field 
+        v-model="state.positionValue" 
+        label="" 
+        placeholder="请选择您的职位类型" 
+        readonly
+        is-link
+        @click="state.showPositionType = true"
+      />
+    </div>
+    <div class="user-item">
+      <h5>工作时间</h5>
+      <van-field v-model="state.companyName" label="" placeholder="请填写您的公司名称" />
+    </div>
+  </div>
   <button class="wy-submit" @click="submit">保存</button>
 </template>
 
 <script setup lang="ts">
 import { myStore } from '@/store/my'
-import { resumeSkill } from '@/api/my'
 import { showToast } from 'vant'
 
 const {closeChange} = inject('popup')
 
 const store = myStore()
 const state = reactive({
-  value: [],
-  show: false,
-  skillList: [],
-  sysSkill: []
+  showPositionType: false,
+  showStartTime: false,
+  showEndTime: false,
+  positionValue: '',
+  companyName: '',
+  startTime: '',
+  endTime: '',
+  workDesc: '',
+  minDate: new Date(1980,1,1),
+  maxDate: new Date()
+})
+const props = defineProps({
+  item: {
+    type: Object
+  }
 })
 
 const setInfo = () => {
-  if(store.resumeInfo.sys_skill_ids) {
-    var arr = []
-    var skill = store.resumeInfo.sys_skill_ids.split(',')
-    for(var i = 0; i < store.sysSkillList.length; i++){
-      var flag = false
-      for(var j = 0; j < skill.length; j++){
-        if(skill[j] === store.sysSkillList[i].title){
-          flag = true
-          break;
-        }
-      }
-      arr.push({
-        title: store.sysSkillList[i].title,
-        active: flag
-      })
-    }
-    state.sysSkill = arr
-  }
-  if(store.resumeInfo.skill_ids){
-    var arr = []
-    var skill = store.resumeInfo.skill_ids.split(',')
-    for(var i = 0;i<skill.length;i++){
-      arr.push({
-        title: skill[i],
-        active: true
-      })
-    }
-    state.skillList = arr
-  }
+  state.positionValue = props.item.work_position
+  state.companyName = props.item.company_name
 }
+setInfo()
 const submit = async() => {
-  var arr = []
-  for(var i =0;i<state.skillList.length;i++){
-    if(state.skillList[i].active){
-      arr.push(state.skillList[i].title)
-    }
-  }
-  var sysArr = []
-  for(var j = 0;j<state.sysSkill.length;j++){
-    if(state.sysSkill[j].active){
-      sysArr.push(state.sysSkill[j].title)
-    }
-  }
-  if(sysArr.length===0){
-    showToast('请选择一项非自定义技能')
-    return
-  }
-  const res = await resumeSkill({
-    "skill_ids": String(arr), 
-    "sys_skill_ids": String(sysArr) 
-  })
-  if(res){
-    showToast('保存成功')
-    closeChange()
-  }else{
-    showToast('保存失败')
-  }
+  
 }
-const setSysSkill = (index) => {
-  state.sysSkill[index].active = !state.sysSkill[index].active
-}
-const setSkill = (index) => {
-  state.skillList[index].active = !state.skillList[index].active
-}
-const skillChange = () => {
-  state.skillList.push({
-    title: state.value,
-    acitve: false
-  })
-  state.value = ''
-  state.show = false
-}
-const clearSkill = (index) => {
-  state.skillList.splice(index,1)
-}
-if(store.sysSkillList.length === 0){
-  (async function(){
-    await store.getLabelsSkill()
-    setInfo()
-  })()
-} else {
-  setInfo()
-}
+
 </script>
 
 <style lang="less" scoped>
-.skill-list{
-  padding: 1.31rem 1.09rem;
-  dt{
-    min-width: 4.5rem;
-    height: 1.17rem;
-    line-height: 1.17rem;
-    background: #FFFFFF;
-    border: 1px solid #E6E6E6;
-    border-radius: 0.27rem;
-    font-size: 0.64rem;
-    font-weight: 400;
-    color: #333333;
-    text-align: center;
-    display: inline-block;
-    margin: 0 0.64rem 0.8rem 0;
-    &.active{
-      border: 1px solid #FF8E00;
-      color: #FF8E00;
-    }
-    i{
-      margin-left: 0.2rem;
-    }
+.position-list{
+  margin: 1.52rem 0.67rem 0;
+  .user-item{
+
   }
 }
 </style>
