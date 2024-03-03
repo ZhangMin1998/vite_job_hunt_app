886c68a427f1176f90405a58292a09e39d6efd66
ZhangMin1998 <1464253423@qq.com>
2024-03-03 22:05:59 +0800

    认证状态：基于组件化思想设计与实现认证状态组件及数据渲染

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -53,7 +53,10 @@
       <div class="my_title">
         <h3>常用功能</h3>
       </div>
-      <div class="my_item"  @click="gotoPage('/my/user/authReal')">
+      <div 
+        class="my_item"
+        @click="gotoPage(store.userInfo.user_is_check === 1 ? '/my/user/certified' : '/my/user/authReal')"
+      >
         <img src="@/assets/img/my/icon-real-name-auth.png" />
         <label>实名认证</label>
         <span><van-icon name="arrow" /></span>
---
+++ src/views/my/user/certified.vue
@@ -0,0 +1,69 @@
+<template>
+  <van-icon class="back" name="arrow-left" @click="gotoBack" />
+  <img class="cert-pic" src="@/assets/img/my/auth-real-certified.png" />
+  <h5>已完成实名认证</h5>
+  <van-field :model-value="store.userInfo.real_name" label="姓名" readonly placeholder="请输入真实姓名" />
+  <van-field :model-value="store.userInfo.idcard" label="证件号码" readonly placeholder="请输入证件号码" />
+  <div class="cert-bottom">
+    <img src="@/assets/img/my/auth-real-safe.png" />
+    <p>信息安全保护中，未经您授权不对外提供</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { myStore } from '@/store/my'
+
+const store = myStore()
+
+const state = reactive({
+  name: '',
+  number: '',
+  idCardJust: '',
+  idCardBack: ''
+})
+const gotoBack = () => history.back()
+</script>
+
+<style lang="less" scoped>
+:deep(.van-field__control){
+  text-align: right;
+}
+.back{
+  position: absolute;
+  top: 1rem;
+  left: 0.69rem;
+  font-size: 1rem;
+  color: #ffffff;
+}
+.cert-pic{
+  width: 100%;
+}
+h5{
+  font-size: 0.94rem;
+  font-weight: 400;
+  color: #FFFFFF;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 13.5rem;
+}
+.cert-bottom{
+  text-align: center;
+  position: absolute;
+  bottom: 2rem;
+  left: 0;
+  width: 100%;
+  img{
+    width: 0.93rem;
+    height: 1.07rem;
+    margin-bottom: 0.72rem;
+  }
+  p{
+    font-size: 0.61rem;
+    line-height: 0.61rem;
+    font-weight: 400;
+    color: #999999;
+    text-align: center;
+  }
+}
+</style>
