d1c7918313069f21fdcf1d4dca39121b099050c1
ZhangMin1998 <1464253423@qq.com>
2024-03-18 22:19:54 +0800

    身份切换：基于组件化思想设计与实现身份列表组件

---
+++ src/views/my/components/SwitchList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="swith-item">
+    <div class="item-cont">
+      <img src="@/assets/img/my/icon-personnel.png" />
+      <div>
+        <h5>IT企业人才</h5>
+        <p>未申请</p>
+      </div>
+      <img class="item-back" src="@/assets/img/my/personnel-bg.png" />
+      <strong>当前身份</strong>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.swith-item{
+  .item-cont{
+    display: flex;
+    align-items: center;
+  }
+  h5{
+
+  }
+  p{
+    
+  }
+  img{
+
+  }
+  strong{
+
+  }
+}
+</style>
